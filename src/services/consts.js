e998f7afd207524b174f861a3113f83b83ae898c
Isaque Hoffmeister <isaque.ha@gmail.com>
2021-02-13 02:20:19 -0300

    fix: export warning

--- src/services/consts.js
+++ src/services/consts.js
@@ -1,7 +1,9 @@
-export default {
+const consts = {
   baseUrl: "https://api.themoviedb.org/3",
   apiKey: "df10f55ba401542cb5f85c11073b7d6e",
   urlImageW500: "https://image.tmdb.org/t/p/w500/",
   urlImageOriginal: "https://image.tmdb.org/t/p/original/",
   urlIMDBTitle: "https://www.imdb.com/title/",
 };
+
+export default consts;
