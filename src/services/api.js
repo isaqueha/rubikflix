d10708f05d2afb260f611aa0fd1df23c4d0fbf6a
Isaque Hoffmeister <isaque.ha@gmail.com>
2021-02-13 02:18:23 -0300

    feat: consts file

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,14 @@
 import axios from "axios";
+import consts from "./consts";
 
 const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
+  baseURL: consts.baseUrl,
   params: {
-    api_key: 'df10f55ba401542cb5f85c11073b7d6e',
+    api_key: consts.apiKey,
   },
   headers: {
-    'Content-Type': 'application/json;charset=utf-8',
+    "Content-Type": "application/json;charset=utf-8",
   },
 });
-
-// api.defaults.withCredentials = true;
 
 export default api;
---
+++ src/services/consts.js
@@ -0,0 +1,7 @@
+export default {
+  baseUrl: "https://api.themoviedb.org/3",
+  apiKey: "df10f55ba401542cb5f85c11073b7d6e",
+  urlImageW500: "https://image.tmdb.org/t/p/w500/",
+  urlImageOriginal: "https://image.tmdb.org/t/p/original/",
+  urlIMDBTitle: "https://www.imdb.com/title/",
+};
